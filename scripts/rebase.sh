0b03ae661a4273afa43c2447cc1f7076645a13bf
Quan Trinh <44539970+QuanTrinhCA@users.noreply.github.com>
2025-07-11 19:53:01 +0000

    Refactor to support more images

--- scripts/rebase.sh
+++ scripts/rebase.sh
@@ -6,10 +6,19 @@ if [ "$EUID" -ne 0 ]; then
   exec sudo "$0" "$@"
 fi
 
+# Take in argument for variant
+VARIANT=$1
+if [[ "$VARIANT" != "fedora-kinoite" && "$VARIANT" != "fedora-silverblue" ]]; then
+  echo "Usage: $0 <variant>" >&2
+  echo "Where <variant> is either 'fedora-kinoite' or 'fedora-silverblue'" >&2
+  exit 1
+fi
+echo "Selected variant: $VARIANT"
+
 # Download container policy public key
 echo "Downloading public key..."
-wget -q "https://raw.githubusercontent.com/quanttrinh/custom-fedora/main/shared/keys/ghcr.io-quanttrinh-fedora-kinoite.pub" \
-     -O /etc/pki/containers/ghcr.io-quanttrinh-fedora-kinoite.pub
+wget -q "https://raw.githubusercontent.com/quanttrinh/custom-fedora/main/shared/pki/ghcr.io-quanttrinh-custom-fedora.pub" \
+     -O /etc/pki/containers/ghcr.io-quanttrinh-custom-fedora.pub
 
 # Restore SELinux context
 echo "Restoring SELinux context..."
@@ -25,8 +34,8 @@ fi
 
 # Execute the policy script
 echo "Applying container policy..."
-./"$FILE"
+./"$FILE" "$VARIANT"
 
 # Rebase to the new image
 echo "Rebasing to new image..."
-rpm-ostree rebase ostree-image-signed:registry:ghcr.io/quanttrinh/fedora-kinoite:latest
+rpm-ostree rebase ostree-image-signed:registry:ghcr.io/quanttrinh/$VARIANT:latest
