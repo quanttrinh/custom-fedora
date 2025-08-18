13456138adc70a6b13f1cc7c1c80d0f8d4737492
Quan Trinh <44539970+quanttrinh@users.noreply.github.com>
2025-08-18 01:39:08 +0000

    Tweaks packages to be installed

--- scripts/common/install_multimedia.sh
+++ scripts/common/install_multimedia.sh
@@ -2,12 +2,7 @@
 set -euo pipefail
 
 dnf swap -y mesa-va-drivers mesa-va-drivers-freeworld
-dnf swap -y mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
 dnf swap -y mesa-vulkan-drivers mesa-vulkan-drivers-freeworld
-
-dnf swap -y mesa-va-drivers.i686 mesa-va-drivers-freeworld.i686
-dnf swap -y mesa-vdpau-drivers.i686 mesa-vdpau-drivers-freeworld.i686
-dnf swap -y mesa-vulkan-drivers.i686 mesa-vulkan-drivers-freeworld.i686
 
 dnf install -y --best --allowerasing \
   --exclude=gstreamer1-*-devel \
@@ -21,10 +16,8 @@ dnf install -y --best --allowerasing \
   libavcodec-freeworld \
   pipewire-codec-aptx \
   ffmpegthumbnailer \
-  gstreamer1-plugins-bad-free-extras \
   gstreamer1-plugins-bad-freeworld \
-  gstreamer1-plugins-good-extras \
-  gstreamer1-plugins-good-gtk \
+  gstreamer1-plugins-good \
   gstreamer1-plugins-ugly \
   gstreamer1-plugin-openh264 \
   gstreamer1-libav \
